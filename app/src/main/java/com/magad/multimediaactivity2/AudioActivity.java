a48727d06efd8e52f8da8256363ccee2e77d9419
TB <habibthea2015@gmail.com>
2019-03-01 23:29:03 +0700

    Initial commit

---
+++ app/src/main/java/com/magad/multimediaactivity2/AudioActivity.java
@@ -0,0 +1,117 @@
+package com.magad.multimediaactivity2;
+
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.IOException;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class AudioActivity extends AppCompatActivity {
+
+    MediaPlayer player;
+
+    @BindView(R.id.btn_Play)
+    Button btnPlay;
+    @BindView(R.id.btn_Stop)
+    Button btnStop;
+    @BindView(R.id.btn_Pause)
+    Button btnPause;
+    @BindView(R.id.btn_Resume)
+    Button btnResume;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_audio);
+        ButterKnife.bind(this);
+
+        player = new MediaPlayer();
+
+        enable(btnPlay);
+        disable(btnResume, btnStop, btnPause);
+    }
+
+    @OnClick({R.id.btn_Play, R.id.btn_Stop, R.id.btn_Pause, R.id.btn_Resume})
+    public void onViewClicked(View view) {
+
+        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.dhuha);
+        player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        switch (view.getId()) {
+            case R.id.btn_Play:
+                player = new MediaPlayer();
+
+                player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+                try {
+                    player.setDataSource(this, uri);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }catch (IllegalStateException o){
+
+                }
+                try {
+                    player.prepare();
+                } catch (IOException e) {
+                    e.printStackTrace();
+
+                }
+
+                player.start();
+
+                disable(btnPlay,btnResume);
+                enable(btnStop,btnPause);
+
+                break;
+            case R.id.btn_Stop:
+                if (player.isPlaying() && player != null){
+                    player.stop();
+                    player.reset();
+                    player.release();
+                    disable(btnStop,btnPause,btnResume);
+                    enable(btnPlay);
+
+
+                }
+
+                break;
+            case R.id.btn_Pause:
+                if(player.isPlaying() && player != null){
+
+
+                player.pause();
+                disable(btnPause,btnPlay);
+                enable(btnStop,btnResume);
+                }
+                break;
+            case R.id.btn_Resume:
+                if(player.isPlaying() && player != null){
+                    player.start();
+
+                    disable(btnStop,btnPause);
+                    enable(btnPlay,btnResume);
+
+                }
+                break;
+        }
+    }
+
+    private void enable(View... views) {
+        for (View v : views) {
+            v.setEnabled(true);
+        }
+    }
+
+    private void disable(View... views) {
+        for (View v : views) {
+            v.setEnabled(false);
+        }
+    }
+}
